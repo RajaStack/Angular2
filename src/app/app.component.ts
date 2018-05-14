4afe49d5a7c21880e213c7b10b504c4395957931
RajaStack <39199396+RajaStack@users.noreply.github.com>
2018-05-14 17:22:45 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { SUBAPI } from './constant/constant.apis';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
 
